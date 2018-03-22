cc64584a60c271019590d81cac38f5e28f4ee56b
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2018-03-22 17:55:20 +0800

    Fix function redeclare issues in php nightly/master

--- src/is_countable.php
+++ src/is_countable.php
@@ -1,16 +1,14 @@
 <?php
 
-if (function_exists('is_countable')) {
-  return;
-}
-
-/**
- * Verify that the content of a variable is an array or an object
- * implementing Countable
- *
- * @param mixed $var The value to check.
- * @return bool Returns TRUE if var is countable, FALSE otherwise.
- */
-function is_countable( $var ) {
-  return (is_array($var) || $var instanceof Countable);
-}
+if (!function_exists('is_countable')):
+  /**
+   * Verify that the content of a variable is an array or an object
+   * implementing Countable
+   *
+   * @param mixed $var The value to check.
+   * @return bool Returns TRUE if var is countable, FALSE otherwise.
+   */
+  function is_countable( $var ) {
+    return (is_array($var) || $var instanceof Countable);
+  }
+endif;
