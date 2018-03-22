2d91234638112c3a5aec2d98c8c991d7a3410f54
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2018-03-22 17:25:09 +0800

    Add initial polyfill

---
+++ src/is_countable.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+if (function_exists('is_countable')) {
+  return;
+}
+
+/**
+ * Verify that the content of a variable is an array or an object
+ * implementing Countable
+ *
+ * @param mixed $var The value to check.
+ * @return bool Returns TRUE if var is countable, FALSE otherwise.
+ */
+function is_countable( $var ) {
+  return (is_array($var) || $var instanceof Countable);
+}
